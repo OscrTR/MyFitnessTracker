61890b508bdf174c4daef853c39330ccc0b435f6
Oscar Thiébaut-Rizzoni <oscar.thiebaut@proton.me>
2024-11-10 16:45:33 +0100

    refactor: training exercise

--- lib/features/training_management/presentation/widgets/exercise_widget.dart
+++ lib/features/training_management/presentation/widgets/exercise_widget.dart
@@ -29,6 +29,7 @@ class _ExerciseWidgetState extends State<ExerciseWidget> {
   final _specialInstructionsController = TextEditingController();
   final _objectivesController = TextEditingController();
   final List<String> setsChoices = ['Reps', 'Duration'];
+
   String? selectedSetsChoice = 'Reps';
 
   @override
@@ -55,274 +56,19 @@ class _ExerciseWidgetState extends State<ExerciseWidget> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                'Exercise',
-                style: TextStyle(color: AppColors.lightBlack),
-              ),
-              MoreWidget(widgetId: widget.widgetId)
-            ],
-          ),
-          DropdownSearch<Exercise>(
-            items: (f, cs) {
-              final currentState = context.read<ExerciseManagementBloc>().state;
-              if (currentState is ExerciseManagementLoaded) {
-                return currentState.exercises;
-              }
-              return [];
-            },
-            compareFn: (Exercise? exercise, Exercise? selectedExercise) =>
-                exercise?.id == selectedExercise?.id,
-            itemAsString: (Exercise? exercise) => exercise?.name ?? '',
-            decoratorProps: DropDownDecoratorProps(
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  borderSide: const BorderSide(
-                    color: AppColors
-                        .lightBlack, // Set color for the enabled border
-                    width: 1,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  borderSide: const BorderSide(
-                    color: AppColors.black, // Set color for the focused border
-                    width: 1,
-                  ),
-                ),
-                hintText: 'No exercise selected yet',
-                hintStyle: Theme.of(context)
-                    .textTheme
-                    .bodyMedium!
-                    .copyWith(color: AppColors.lightBlack),
-              ),
-            ),
-            onChanged: (selectedItem) {
-              context
-                  .read<ExerciseManagementBloc>()
-                  .add(GetExerciseEvent(selectedItem!.id!));
-            },
-            popupProps: PopupProps.menu(
-              menuProps: const MenuProps(
-                  elevation: 0, backgroundColor: Colors.transparent),
-              showSearchBox: true,
-              searchFieldProps: TextFieldProps(
-                decoration: InputDecoration(
-                  hintText: 'Search...',
-                  hintStyle: Theme.of(context)
-                      .textTheme
-                      .bodyMedium!
-                      .copyWith(color: AppColors.lightBlack),
-                  enabledBorder: const UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: AppColors
-                          .lightBlack, // Search box enabled border color
-                      width: 1,
-                    ),
-                  ),
-                  focusedBorder: const UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: AppColors
-                          .lightBlack, // Search box focused border color
-                      width: 1,
-                    ),
-                  ),
-                ),
-              ),
-              containerBuilder: (context, popupWidget) {
-                return Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  decoration: BoxDecoration(
-                    color: AppColors.white,
-                    borderRadius: BorderRadius.circular(15),
-                    border: Border.all(
-                      color: AppColors.black,
-                      width: 1,
-                    ),
-                  ),
-                  child: popupWidget,
-                );
-              },
-            ),
-          ),
+          _buildHeader(),
+          _buildExerciseDropdown(),
           const SizedBox(height: 10),
-          BlocBuilder<ExerciseManagementBloc, ExerciseManagementState>(
-              builder: (context, state) {
-            if (state is ExerciseManagementLoaded) {
-              if (state.selectedExercise != null) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        border:
-                            Border.all(color: AppColors.lightBlack, width: 1.0),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        child: state.selectedExercise!.imagePath != ''
-                            ? Image.file(
-                                File(state.selectedExercise!.imagePath!),
-                                width: MediaQuery.of(context).size.width - 40,
-                                fit: BoxFit.cover,
-                              )
-                            : Container(
-                                color: AppColors.lightGrey,
-                              ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                      state.selectedExercise!.description ?? '',
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodySmall!
-                          .copyWith(color: AppColors.lightBlack),
-                    ),
-                  ],
-                );
-              }
-              return const SizedBox();
-            }
-            return const Text("Exercises couldn't load");
-          }),
+          _buildExerciseDetails(),
           const SizedBox(height: 10),
           const Divider(
             color: AppColors.lightBlack,
           ),
           const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text('Sets', style: TextStyle(color: AppColors.lightBlack)),
-              SmallTextFieldWidget(
-                controller: _setsController,
-              )
-            ],
-          ),
-          Column(
-            children: [
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    selectedSetsChoice = 'Reps';
-                  });
-                },
-                child: Row(
-                  children: [
-                    ClipRect(
-                      child: SizedBox(
-                        width: 20,
-                        child: Radio(
-                            fillColor: WidgetStateProperty.resolveWith<Color>(
-                                (Set<WidgetState> states) {
-                              return selectedSetsChoice == 'Reps'
-                                  ? AppColors.black
-                                  : AppColors.lightBlack;
-                            }),
-                            value: 'Reps',
-                            groupValue: selectedSetsChoice,
-                            onChanged: (value) {
-                              setState(() {
-                                selectedSetsChoice = value;
-                              });
-                            }),
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text('Reps',
-                              style: TextStyle(color: AppColors.lightBlack)),
-                          Row(
-                            children: [
-                              SmallTextFieldWidget(
-                                  controller: _repsMinController),
-                              const Text(
-                                '  -  ',
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              SmallTextFieldWidget(
-                                  controller: _repsMaxController),
-                            ],
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    selectedSetsChoice = 'Duration';
-                  });
-                },
-                child: Row(
-                  children: [
-                    ClipRect(
-                      child: SizedBox(
-                        width: 20,
-                        child: Radio(
-                            fillColor: WidgetStateProperty.resolveWith<Color>(
-                                (Set<WidgetState> states) {
-                              return selectedSetsChoice == 'Duration'
-                                  ? AppColors.black
-                                  : AppColors.lightBlack;
-                            }),
-                            value: 'Duration',
-                            groupValue: selectedSetsChoice,
-                            onChanged: (value) {
-                              setState(() {
-                                selectedSetsChoice = value;
-                              });
-                            }),
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            'Duration (seconds)',
-                            style: TextStyle(color: AppColors.lightBlack),
-                          ),
-                          SmallTextFieldWidget(controller: _durationController)
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text('Set rest (seconds)',
-                  style: TextStyle(color: AppColors.lightBlack)),
-              SmallTextFieldWidget(
-                controller: _setRestController,
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text('Exercise rest (seconds)',
-                  style: TextStyle(color: AppColors.lightBlack)),
-              SmallTextFieldWidget(
-                controller: _exerciseRestController,
-              )
-            ],
-          ),
+          _buildSetsRow(),
+          _buildSetsChoiceOptions(),
+          _buildSetRestRow(),
+          _buildExerciseRestRow(),
           const SizedBox(height: 10),
           BigTextFieldWidget(
               controller: _specialInstructionsController,
@@ -332,6 +78,229 @@ class _ExerciseWidgetState extends State<ExerciseWidget> {
               controller: _objectivesController, hintText: 'Objectives')
         ],
       ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text('Exercise', style: TextStyle(color: AppColors.lightBlack)),
+        MoreWidget(widgetId: widget.widgetId),
+      ],
+    );
+  }
+
+  Widget _buildExerciseDropdown() {
+    return DropdownSearch<Exercise>(
+      items: (f, cs) {
+        final currentState = context.read<ExerciseManagementBloc>().state;
+        if (currentState is ExerciseManagementLoaded) {
+          return currentState.exercises;
+        }
+        return [];
+      },
+      compareFn: (Exercise? exercise, Exercise? selectedExercise) =>
+          exercise?.id == selectedExercise?.id,
+      itemAsString: (Exercise? exercise) => exercise?.name ?? '',
+      onChanged: (selectedItem) {
+        context
+            .read<ExerciseManagementBloc>()
+            .add(GetExerciseEvent(selectedItem!.id!));
+      },
+      decoratorProps: DropDownDecoratorProps(
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: const BorderSide(
+              color: AppColors.lightBlack,
+              width: 1,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: const BorderSide(
+              color: AppColors.black,
+              width: 1,
+            ),
+          ),
+          hintText: 'No exercise selected yet',
+          hintStyle: Theme.of(context)
+              .textTheme
+              .bodyMedium!
+              .copyWith(color: AppColors.lightBlack),
+        ),
+      ),
+      popupProps: PopupProps.menu(
+        menuProps:
+            const MenuProps(elevation: 0, backgroundColor: Colors.transparent),
+        showSearchBox: true,
+        searchFieldProps: TextFieldProps(
+          decoration: InputDecoration(
+            hintText: 'Search...',
+            hintStyle: Theme.of(context)
+                .textTheme
+                .bodyMedium!
+                .copyWith(color: AppColors.lightBlack),
+            enabledBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: AppColors.lightBlack,
+                width: 1,
+              ),
+            ),
+            focusedBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: AppColors.lightBlack,
+                width: 1,
+              ),
+            ),
+          ),
+        ),
+        containerBuilder: (context, popupWidget) {
+          return Container(
+            margin: const EdgeInsets.only(top: 10),
+            decoration: BoxDecoration(
+              color: AppColors.white,
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(
+                color: AppColors.black,
+                width: 1,
+              ),
+            ),
+            child: popupWidget,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildExerciseDetails() {
+    return BlocBuilder<ExerciseManagementBloc, ExerciseManagementState>(
+      builder: (context, state) {
+        if (state is ExerciseManagementLoaded &&
+            state.selectedExercise != null) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildExerciseImage(state.selectedExercise!.imagePath),
+              const SizedBox(height: 10),
+              Text(
+                state.selectedExercise!.description ?? '',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodySmall!
+                    .copyWith(color: AppColors.lightBlack),
+              ),
+            ],
+          );
+        }
+        return const Text("Exercises couldn't load");
+      },
+    );
+  }
+
+  Widget _buildExerciseImage(String? imagePath) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColors.lightBlack, width: 1),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: imagePath != null && imagePath.isNotEmpty
+            ? Image.file(File(imagePath),
+                width: MediaQuery.of(context).size.width - 40,
+                fit: BoxFit.cover)
+            : Container(color: AppColors.lightGrey),
+      ),
+    );
+  }
+
+  Widget _buildSetsRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text('Sets', style: TextStyle(color: AppColors.lightBlack)),
+        SmallTextFieldWidget(controller: _setsController),
+      ],
+    );
+  }
+
+  Widget _buildSetsChoiceOptions() {
+    return Column(
+      children: [
+        _buildSetsChoiceOption('Reps', _repsMinController, _repsMaxController),
+        _buildSetsChoiceOption('Duration', _durationController),
+      ],
+    );
+  }
+
+  Widget _buildSetsChoiceOption(String choice,
+      [TextEditingController? controller1,
+      TextEditingController? controller2]) {
+    return GestureDetector(
+      onTap: () => setState(() => selectedSetsChoice = choice),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 20,
+            child: Radio<String>(
+              value: choice,
+              groupValue: selectedSetsChoice,
+              onChanged: (value) => setState(() => selectedSetsChoice = value!),
+              activeColor: AppColors.black,
+              fillColor: WidgetStateProperty.resolveWith<Color>(
+                  (Set<WidgetState> states) {
+                return selectedSetsChoice == choice
+                    ? AppColors.black
+                    : AppColors.lightBlack;
+              }),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(choice,
+                    style: const TextStyle(color: AppColors.lightBlack)),
+                Row(
+                  children: [
+                    if (controller1 != null)
+                      SmallTextFieldWidget(controller: controller1),
+                    if (controller2 != null)
+                      const Text(' - ', style: TextStyle(fontSize: 20)),
+                    if (controller2 != null)
+                      SmallTextFieldWidget(controller: controller2),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSetRestRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text('Set rest (seconds)',
+            style: TextStyle(color: AppColors.lightBlack)),
+        SmallTextFieldWidget(controller: _setRestController),
+      ],
+    );
+  }
+
+  Widget _buildExerciseRestRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text('Exercise rest (seconds)',
+            style: TextStyle(color: AppColors.lightBlack)),
+        SmallTextFieldWidget(controller: _exerciseRestController),
+      ],
     );
   }
 }
