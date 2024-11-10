ff241fcaea3bcfbbc6a39c4399c6cc648492f3be
Oscar Thiébaut-Rizzoni <oscar.thiebaut@proton.me>
2024-11-10 16:31:23 +0100

    feat: add training exercise

--- lib/features/training_management/presentation/widgets/exercise_widget.dart
+++ lib/features/training_management/presentation/widgets/exercise_widget.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_fitness_tracker/assets/app_colors.dart';
+import 'package:my_fitness_tracker/features/exercise_management/domain/entities/exercise.dart';
+import 'package:my_fitness_tracker/features/exercise_management/presentation/bloc/exercise_management_bloc.dart';
 import 'package:my_fitness_tracker/features/training_management/presentation/widgets/big_text_field_widget.dart';
 import 'package:my_fitness_tracker/features/training_management/presentation/widgets/more_widget.dart';
 import 'package:my_fitness_tracker/features/training_management/presentation/widgets/small_text_field_widget.dart';
@@ -59,9 +65,132 @@ class _ExerciseWidgetState extends State<ExerciseWidget> {
               MoreWidget(widgetId: widget.widgetId)
             ],
           ),
-          // TODO : exercise search
-
-          // TODO : exercise display
+          DropdownSearch<Exercise>(
+            items: (f, cs) {
+              final currentState = context.read<ExerciseManagementBloc>().state;
+              if (currentState is ExerciseManagementLoaded) {
+                return currentState.exercises;
+              }
+              return [];
+            },
+            compareFn: (Exercise? exercise, Exercise? selectedExercise) =>
+                exercise?.id == selectedExercise?.id,
+            itemAsString: (Exercise? exercise) => exercise?.name ?? '',
+            decoratorProps: DropDownDecoratorProps(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: const BorderSide(
+                    color: AppColors
+                        .lightBlack, // Set color for the enabled border
+                    width: 1,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: const BorderSide(
+                    color: AppColors.black, // Set color for the focused border
+                    width: 1,
+                  ),
+                ),
+                hintText: 'No exercise selected yet',
+                hintStyle: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: AppColors.lightBlack),
+              ),
+            ),
+            onChanged: (selectedItem) {
+              context
+                  .read<ExerciseManagementBloc>()
+                  .add(GetExerciseEvent(selectedItem!.id!));
+            },
+            popupProps: PopupProps.menu(
+              menuProps: const MenuProps(
+                  elevation: 0, backgroundColor: Colors.transparent),
+              showSearchBox: true,
+              searchFieldProps: TextFieldProps(
+                decoration: InputDecoration(
+                  hintText: 'Search...',
+                  hintStyle: Theme.of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(color: AppColors.lightBlack),
+                  enabledBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: AppColors
+                          .lightBlack, // Search box enabled border color
+                      width: 1,
+                    ),
+                  ),
+                  focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: AppColors
+                          .lightBlack, // Search box focused border color
+                      width: 1,
+                    ),
+                  ),
+                ),
+              ),
+              containerBuilder: (context, popupWidget) {
+                return Container(
+                  margin: const EdgeInsets.only(top: 10),
+                  decoration: BoxDecoration(
+                    color: AppColors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(
+                      color: AppColors.black,
+                      width: 1,
+                    ),
+                  ),
+                  child: popupWidget,
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 10),
+          BlocBuilder<ExerciseManagementBloc, ExerciseManagementState>(
+              builder: (context, state) {
+            if (state is ExerciseManagementLoaded) {
+              if (state.selectedExercise != null) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border:
+                            Border.all(color: AppColors.lightBlack, width: 1.0),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: state.selectedExercise!.imagePath != ''
+                            ? Image.file(
+                                File(state.selectedExercise!.imagePath!),
+                                width: MediaQuery.of(context).size.width - 40,
+                                fit: BoxFit.cover,
+                              )
+                            : Container(
+                                color: AppColors.lightGrey,
+                              ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      state.selectedExercise!.description ?? '',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodySmall!
+                          .copyWith(color: AppColors.lightBlack),
+                    ),
+                  ],
+                );
+              }
+              return const SizedBox();
+            }
+            return const Text("Exercises couldn't load");
+          }),
+          const SizedBox(height: 10),
           const Divider(
             color: AppColors.lightBlack,
           ),
@@ -69,7 +198,7 @@ class _ExerciseWidgetState extends State<ExerciseWidget> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Sets', style: TextStyle(color: AppColors.lightBlack)),
+              const Text('Sets', style: TextStyle(color: AppColors.lightBlack)),
               SmallTextFieldWidget(
                 controller: _setsController,
               )
