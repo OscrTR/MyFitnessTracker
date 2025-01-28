fe28542cdcd99ca9b0cb2ed45246eedd6464fe57
Oscar Thiébaut-Rizzoni <oscar.thiebaut@proton.me>
2025-01-28 16:22:42 +0100

    feat: empty training homepage

--- lib/features/homepage/presentation/pages/home_page.dart
+++ lib/features/homepage/presentation/pages/home_page.dart
@@ -3,12 +3,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:material_symbols_icons/symbols.dart';
-
 import '../../../../app_colors.dart';
-import '../../../../core/widgets/dash_border_painter_widget.dart';
-import '../../../active_training/presentation/bloc/active_training_bloc.dart';
-import '../../../training_management/domain/entities/training.dart';
 import '../../../training_management/presentation/bloc/training_management_bloc.dart';
 
 class HomePage extends StatefulWidget {
@@ -41,201 +36,99 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           const SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  tr('home_page_selected_trainings'),
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    GoRouter.of(context).push('/trainings');
-                  },
-                  child: Container(
-                      width: 100,
-                      padding: const EdgeInsets.symmetric(vertical: 5),
-                      decoration: BoxDecoration(
-                          color: AppColors.white,
-                          borderRadius: BorderRadius.circular(15)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            tr('home_page_see_all'),
-                            style: const TextStyle(color: AppColors.frenchGray),
-                          ),
-                          const Icon(
-                            Icons.chevron_right,
-                            color: AppColors.frenchGray,
-                          )
-                        ],
-                      )),
-                )
-              ],
-            ),
-          ),
+          _buildTrainingsList(),
           const SizedBox(height: 30),
-          _buildSelectedWidgets(),
         ],
       ),
     );
   }
 }
 
-Widget _buildSelectedWidgets() {
+Widget _buildTrainingsList() {
   return BlocBuilder<TrainingManagementBloc, TrainingManagementState>(
-    builder: (context, state) {
-      if ((state is TrainingManagementInitial)) {
+      builder: (context, state) {
+    if (state is TrainingManagementLoaded) {
+      final plannedTrainings = state.trainings.where((training) =>
+          training.trainingDays != null && training.trainingDays!.isNotEmpty);
+
+      if (plannedTrainings.isEmpty) {
         return Container(
-          height: 200,
+          margin: const EdgeInsets.only(left: 20),
+          width: MediaQuery.of(context).size.width - 40,
           padding: const EdgeInsets.all(20),
           decoration: BoxDecoration(
-              border: Border.all(color: AppColors.whiteSmoke),
-              borderRadius: BorderRadius.circular(15)),
-        );
-      }
-
-      if ((state is TrainingManagementLoaded)) {
-        final filteredTrainings = state.trainings;
-
-        if (filteredTrainings.isEmpty) {
-          return Container(
-            alignment: Alignment.topLeft,
-            padding: const EdgeInsets.only(left: 20),
-            child: GestureDetector(
-              onTap: () {
-                GoRouter.of(context).push('/trainings');
-              },
-              child: CustomPaint(
-                painter: DashedBorderPainter(
-                  color: AppColors.frenchGray,
-                  strokeWidth: 1.0,
-                  dashLength: 5.0,
-                  gapLength: 5.0,
-                ),
-                child: SizedBox(
-                  height: 180,
-                  width: 180,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        textAlign: TextAlign.center,
-                        tr('home_page_select_training'),
-                        style: const TextStyle(color: AppColors.frenchGray),
-                      ),
-                      const SizedBox(width: 5),
-                      const Icon(Icons.add, color: AppColors.frenchGray),
-                    ],
-                  ),
-                ),
+              color: AppColors.floralWhite,
+              border: Border.all(color: AppColors.parchment),
+              borderRadius: BorderRadius.circular(20)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                tr('home_page_first_training_title'),
+                style: Theme.of(context).textTheme.titleLarge,
               ),
-            ),
-          );
-        } else {
-          return Container(
-            padding: const EdgeInsets.only(left: 20),
-            height: 180,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  final training = state.trainings.toList()[index];
-
-                  Color color = AppColors.white;
-                  IconData icon = Icons.self_improvement;
-                  if (training.type == TrainingType.yoga) {
-                    icon = Icons.self_improvement;
-                  } else if (training.type == TrainingType.run) {
-                    icon = Symbols.sprint;
-                  } else if (training.type == TrainingType.workout) {
-                    icon = Icons.fitness_center;
-                  }
-                  return Container(
-                    padding: const EdgeInsets.all(20),
-                    margin: const EdgeInsets.only(right: 10),
-                    width: 180,
-                    decoration: BoxDecoration(
-                        color: color, borderRadius: BorderRadius.circular(15)),
-                    child: Stack(
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Icon(icon),
-                                const SizedBox(width: 10),
-                                Text(training.type.name[0].toUpperCase() +
-                                    training.type.name.substring(1)),
-                              ],
-                            ),
-                            Text(training.name),
-                            GestureDetector(
-                              onTap: () {
-                                context
-                                    .read<ActiveTrainingBloc>()
-                                    .add(LoadDefaultActiveTraining());
-
-                                final trainingManagementBloc =
-                                    context.read<TrainingManagementBloc>();
-                                trainingManagementBloc
-                                    .add(StartTrainingEvent(training.id!));
-
-                                GoRouter.of(context).push('/active_training');
-                              },
-                              child: IntrinsicWidth(
-                                child: Container(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 5, horizontal: 10),
-                                  decoration: BoxDecoration(
-                                      color: AppColors.white,
-                                      borderRadius: BorderRadius.circular(15)),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(tr('global_start')),
-                                      const Icon(Icons.chevron_right)
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Positioned(
-                          top: -10,
-                          right: -12,
-                          child: PopupMenuButton(
-                            onSelected: (value) {
-                              if (value == 'unselect') {}
-                            },
-                            itemBuilder: (BuildContext context) => [
-                              PopupMenuItem(
-                                value: 'unselect',
-                                child: Row(
-                                  children: [Text(tr('home_page_unselect'))],
-                                ),
-                              ),
-                            ],
-                            icon: const Icon(
-                              Icons.more_horiz,
-                              color: AppColors.frenchGray,
-                            ),
+              const SizedBox(height: 3),
+              Text(
+                tr('home_page_first_training_description'),
+                style: Theme.of(context)
+                    .textTheme
+                    .bodySmall!
+                    .copyWith(color: AppColors.taupeGray),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () => GoRouter.of(context).push('/training_detail'),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 7),
+                      decoration: BoxDecoration(
+                          color: AppColors.folly,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.play_arrow_rounded,
+                            color: AppColors.white,
+                            size: 18,
                           ),
+                          const SizedBox(width: 7),
+                          Text(
+                            tr('global_start'),
+                            style: const TextStyle(color: AppColors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+                    decoration: BoxDecoration(
+                        color: AppColors.white,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.calendar_month_rounded,
+                          color: AppColors.licorice,
+                          size: 18,
                         ),
+                        const SizedBox(width: 7),
+                        Text(tr('home_page_planned_today')),
                       ],
                     ),
-                  );
-                },
-                itemCount: state.trainings.length),
-          );
-        }
+                  )
+                ],
+              )
+            ],
+          ),
+        );
       }
-      return Center(child: Text(context.tr('error_state')));
-    },
-  );
+      return Container();
+    }
+    return const SizedBox();
+  });
 }
