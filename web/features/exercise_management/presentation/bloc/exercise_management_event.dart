4380d278c2591ccf02234495dea33c0209370005
Oscar Thiébaut-Rizzoni <oscar.thiebaut@proton.me>
2025-02-27 19:21:34 +0100

    git changes

--- web/features/exercise_management/presentation/bloc/exercise_management_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'exercise_management_bloc.dart';
-
-abstract class ExerciseManagementEvent extends Equatable {
-  const ExerciseManagementEvent();
-
-  @override
-  List<Object> get props => [];
-}
--- web/features/exercise_management/presentation/bloc/exercise_management_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'exercise_management_bloc.dart';
-
-abstract class ExerciseManagementState extends Equatable {
-  const ExerciseManagementState();  
-
-  @override
-  List<Object> get props => [];
-}
-class ExerciseManagementInitial extends ExerciseManagementState {}
